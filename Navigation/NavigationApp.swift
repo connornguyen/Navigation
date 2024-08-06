632e06e1db725400fab788fee0c446146cea1602
Connor Nguyen <nguyendinhthanhcong2512@gmail.com>
2024-08-06 15:56:34 -0700

    First Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Apple on 06/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
