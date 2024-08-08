955d6d38b7d3df3e8e1cd641454cdb70616c7c6e
Connor Nguyen <nguyendinhthanhcong2512@gmail.com>
2024-08-08 11:26:15 -0700

    3/11 progress

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -7,15 +7,37 @@
 
 import SwiftUI
 
-struct ContentView: View {
+
+//Hashable because UUID, string and Int is comform to hashable
+struct Student: Hashable {
+    var id = UUID()
+    var name: String
+    var age: Int
+}
+
+struct DetailView: View {
+    
+    var number: Int
+    @Binding var path: NavigationPath
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationLink("Go to random number", value: Int.random(in: 0...1000))
+            .navigationTitle("Number \(number)")
+    }
+}
+
+
+struct ContentView: View {
+    @State private var path = NavigationPath()
+    
+    var body: some View {
+        NavigationStack(path: $path) {
+            DetailView(number: 0 ,path: $path)
+                .navigationDestination(for: Int.self) { i in
+                    DetailView(number: i, path: $path)
+                }
         }
-        .padding()
     }
 }
 
